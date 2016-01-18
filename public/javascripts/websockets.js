345588e1f332890cab71c1b4e5098f8b36f7fbd1
Marin Marjanovic <marin_marjanovic@me.com>
2016-01-18 13:01:45 +0100

    project added

---
+++ public/javascripts/websockets.js
@@ -0,0 +1,39 @@
+﻿var io = io.connect();
+io.on('announce', function (data) {
+	$('#conversation').append('<button>' + data.message + '</button>');
+});
+
+io.on('userConnected', function (data) {
+	$('#console').append('<p>' + data.message + '</p>');
+});
+
+io.on('lockRoom', function (data) { 
+    $('#conversation').append('<p> lockRoom </p>');
+});
+
+io.on('setQuestion', function (data) {
+    $('#question').append('<p>' + data.message + '</p>');
+    console.log('tu sam');
+});
+
+$('#sendButton').click(function () {
+	
+	var messageText = $('#chatTextBox').val();
+	if (messageText === "" || messageText === "undefined") alert("Nije dozvoljen prazan unos!");
+	else {
+		var messagePayload = { message: messageText, room: roomName, username: userName };
+		io.emit('sendMessage', messagePayload);
+		$('#chatTextBox').val(''); //clears the message text box
+		$('#conversation').append('<button style="color: rgb(191, 62, 17)">' + messagePayload.message + '</button>'); ///adds  message to conversation
+	}
+});
+
+$('#lockButton').click(function (evt) {
+    io.emit('lockRoom', { roomName: roomName});
+    alert('soba zakljucana');
+});
+
+io.on('userDisconected', function (data) {
+	$('#conversation').append('<p>' + data.username + 'has disconnected. </p>');
+        
+});
---
+++ server.js
@@ -0,0 +1,211 @@
+﻿/*var http = require('http');
+var port = process.env.port || 1337;
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);*/
+
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express.io');
+var http = require('http');
+var path = require('path');
+var passport = require('passport'), LocalStrategy = require('passport-local').Strategy;
+var sql = require('mssql');
+
+
+var rooms = ['Public', 'Elfs', 'Random'];
+var lockedRooms = ['Public'];
+var app = express();
+//setup socket.io
+app.http().io()
+
+// all environments
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(require('stylus').middleware(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(app.router);
+
+
+
+//var User = [{ username: "dana", password: "no" }, { username: "admin", password: "yo" }, { username: "dm", password: "dmsecretpass" }];
+var DMSocket;
+var indexMessage = '';
+var config = {
+    server: "localhost\\MSSQLSERVER",
+    database: "LieToMeDB",
+    user: "sa",
+    password:"n4KmgANB"
+};
+
+passport.serializeUser(function (user, done) {
+	done(null, user.id);
+});
+passport.deserializeUser(function (id, done) {
+	User.findById(id, function (err, user) {
+		done(err, user);
+	});
+});
+/*passport.use(new LocalStrategy(function (username, password, done) {
+	if (username === "dana")
+		return done(null, { id: 1, username: "dana", password: "no" });
+}));*/
+
+// development only
+if ('development' == app.get('env')) {
+	app.use(express.errorHandler());
+}
+app.get('/', function (req, res) {
+    
+    res.render('index');
+
+});
+
+app.get('/create', function (req, res) {
+    res.render('createroom', { title: 'LieToMe Room Create' });
+
+});
+/*app.get('/login', function (req, res) {
+	
+	res.render('login', { title: 'DMITAR Chat Room Super Secret DM login' });
+
+});*/
+app.get('/about', function (req, res) {
+	
+	res.render('about', { title: '' });
+
+});
+app.get('/rooms/:id', function (req, res) {
+	if (rooms.indexOf(req.params.id) === -1) {
+		rooms.push(req.params.id);
+		console.log(rooms);
+		DMSocket.join(req.params.id);
+		DMSocket.emit('newRoom', { room: req.params.id });
+    }
+    if (!isInArray(lockedRooms, req.params.id)) {
+        //render the room template with the name of the room for the underlying data model
+        res.render('room', { title : req.params.id, username : req.query.username }); 
+    } else {
+        indexMessage = 'ne bu išlo';
+        res.redirect('home');
+    }
+        
+});
+/*app.get('/dm/:user', function (req, res) {
+	console.log(req.params.user);
+	res.render('dmview', { title: 'Server.', rooms: rooms, user: req.params.user });
+
+});*/
+
+app.get('/server', function (req, res) {
+        
+    res.render('server', { rooms: rooms });
+});
+app.get('/home', function (req, res) {
+    
+    if (indexMessage !== '') {
+        indexMessage = '';
+        res.render('home', { title: 'LieToMe' , rooms: rooms, message: 'ne bu išlo' });
+    } else {
+        res.render('home', { title: 'LieToMe' , rooms: rooms, message: '' });
+    }
+});
+
+app.get('/serverRoom/:id', function (req, res) {
+	if (rooms.indexOf(req.params.id) === -1) {
+		rooms.push(req.params.id);
+		console.log(rooms);
+    }
+    
+    //render the room template with the name of the room for the underlying data model
+    res.render('serverRoom', { title : req.params.id, username: 'root' });
+    
+});
+/*app.post('/login', passport.authenticate('local'), function (req, res) {
+	// If this function gets called, authentication was successful.
+	// `req.user` contains the authenticated user.
+	res.end(req.user.username);
+});*/
+
+// Setup the ready route, join room and broadcast to room.
+app.io.route('userConnected', function (req) {
+	console.log('Recieved real-time ready message from user: ' + req.data.username + ' for room: ' + req.data.room);
+	req.io.join(req.data.room);
+	req.io.room(req.data.room).broadcast('userConnected', {
+		message: req.data.username + ' just joined the room. ',
+		username: req.data.username
+	});
+});
+app.io.route('sendMessage', function (req) {
+	
+	console.log('recieved message : ' + req.data.message + ' for room ' + req.data.room + ' from user ' + req.data.username);
+	req.io.join(req.data.room);
+	req.io.room(req.data.room).broadcast('announce', {
+		message: req.data.message,
+		username: req.data.username,
+		room: req.data.room
+	});
+});
+app.io.route('lockRoom', function (req) {
+    
+    lockedRooms.push(req.data.roomName);
+    /*
+    var questions = ['proba'];
+    var conn = new sql.Connection(config);
+    var request = new sql.Request(conn);
+    
+    conn.connect(function (err) {
+        if (err) {
+            console.log(err);
+            return;
+        };
+        request.query("SELECT * FROM Question", function (err, recordset) {
+            if (err) {
+                console.log(err);
+            }
+            else {
+                //questions = recordset;
+                console.log(recordset);
+            }
+            conn.close();
+        });
+    });*/
+    console.log('roomname=' + req.data.roomName);
+    
+    req.io.join(req.data.roomName);
+    req.io.room(req.data.roomName).broadcast('announce', {
+        message: "lol",
+        username: "root",
+        room: req.data.roomName
+    });
+});
+/*app.io.route('DMWatching', function (req) {
+	rooms.forEach(function (room) {
+		req.io.join(room);
+	});
+	console.log(req.io.socket);
+	DMSocket = req.io;
+});*/
+
+
+app.listen(app.get('port'));
+
+function isInArray(array, obj) {
+    for (var i = 0; i < array.length; i++) {
+        if (array[i] === obj)
+            return true;
+    }
+    return false;
+}
